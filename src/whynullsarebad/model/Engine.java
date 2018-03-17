6fbd53c20fc6d8d8d9b85a45284572d6aa3dae9e
Pedro Rosanes <prosanes@gmail.com>
2018-03-17 13:26:37 -0300

    Solve problem with NonNull lombok

--- src/whynullsarebad/model/Engine.java
+++ src/whynullsarebad/model/Engine.java
@@ -2,10 +2,14 @@ package whynullsarebad.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
 @Data
 public class Engine {
+    @NonNull
     private Cylinders cylinders;
+    @NonNull
     private Integer size;
 }
